093921983e98c14dfe3b416fe38391c7cc544cf3
Shreya Naik <naik.shreya2708@gmail.com>
2017-04-01 17:32:10 +0530

    Static files

--- keystroke_dynamics/app/views.py
+++ keystroke_dynamics/app/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render_to_response
 def login(request):
 	return render(request,'login.html')
 
+
+def index(request):
+	return render(request,'index.html')
+
+
--- keystroke_dynamics/keystroke_dynamics/urls.py
+++ keystroke_dynamics/keystroke_dynamics/urls.py
@@ -20,4 +20,5 @@ from app import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/', views.login),
+    url(r'^index/', views.index),
 ]
